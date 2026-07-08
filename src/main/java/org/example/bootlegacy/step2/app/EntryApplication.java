6e929692cc56c49c4f155ecd5ee162c851b55a77
a1l1ke <onetenthplease@gmail.com>
2026-07-08 12:42:48 +0900

    fix: EntryApplication 주석 처리로 중복 메인 클래스 빌드 에러 해결

--- src/main/java/org/example/bootlegacy/step2/app/EntryApplication.java
+++ src/main/java/org/example/bootlegacy/step2/app/EntryApplication.java
@@ -3,11 +3,11 @@ package org.example.bootlegacy.step2.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EntryApplication {
-
-    // 경로 상 ScanController를 스프링 컨테이너 등록 실패해서 작동 X.
-    public static void main(String[] args) {
-        SpringApplication.run(EntryApplication.class, args);
-    }
-}
+//@SpringBootApplication
+//public class EntryApplication {
+//
+//    // 경로 상 ScanController를 스프링 컨테이너 등록 실패해서 작동 X.
+////    public static void main(String[] args) {
+////        SpringApplication.run(EntryApplication.class, args);
+////    }
+//}
